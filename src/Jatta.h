01a5d2295642bf3d1d78d81d2375f6a859acf2d7
Joshua Brookover <josh@jalb.me>
2012-12-20 23:42:13 -0600

    Uh... crisis averted.

--- src/Jatta.h
+++ src/Jatta.h
@@ -1,5 +1,17 @@
 #pragma once
 
+#if !defined(WINDOWS) and !defined(LINUX) and !defined(MACOS)
+#  if defined(_WIN32)
+#    define WINDOWS
+#  elif defined(__APPLE__)
+#    define MACOS
+#  elif defined(unix)
+#    define LINUX
+#  else
+#    error Failed to automatically detected operating system.  Must define WINDOWS, LINUX, or MACOS on the command line.
+#  endif
+#endif
+
 // Standard Template Library
 #include <array>
 #include <fstream>
