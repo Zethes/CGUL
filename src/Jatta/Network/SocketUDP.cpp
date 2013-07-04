72c5a3c21744bb7c583def6c41fc0b01bdc30f0e
Joshua Brookover <josh@jalb.me>
2013-07-03 23:34:29 -0500

    Didnt mean to commit the test example. Also the inet_ntop hack is no longer neededs, removing that.

--- src/Jatta/Network/SocketUDP.cpp
+++ src/Jatta/Network/SocketUDP.cpp
@@ -18,34 +18,6 @@ namespace Jatta
     }
 }
 
-// TODO: come up with a more permanent place for this
-#if defined(WINDOWS) && !defined(MSVC)
-static const char* inet_ntop(int af, const void *src, char *dst, socklen_t cnt)
-{
-  if (af == AF_INET) {
-    struct sockaddr_in in;
-    memset(&in, 0, sizeof(in));
-    in.sin_family = AF_INET;
-    memcpy(&in.sin_addr, src, sizeof(struct in_addr));
-    getnameinfo((struct sockaddr *)&in, sizeof(struct sockaddr_in), dst, cnt,
-                NULL, 0, NI_NUMERICHOST);
-    return dst;
-  }
-  else if (af == AF_INET6) {
-    struct sockaddr_in6 in;
-    memset(&in, 0, sizeof(in));
-    in.sin6_family = AF_INET6;
-    memcpy(&in.sin6_addr, src, sizeof(struct in_addr6));
-    getnameinfo((struct sockaddr *)&in, sizeof(struct sockaddr_in6), dst, cnt,
-                NULL, 0, NI_NUMERICHOST);
-    return dst;
-  }
-  return NULL;
-}
-#endif
-
-
-
 /** @brief Makes the socket a non-blocking socket.
  *  @details This happens to all sockets created.  This class does not supported blocking sockets.
  */
