bea093b5c7db771b3583881559a647a41e413e46
Joshua Brookover <josh@jalb.me>
2013-07-11 22:40:25 -0500

    Made improvements to the cmake lists and added support for packaging. Also, sweet logos thanks to Mordi (Landeplage on github)!

---
+++ Doxygen.cmake
@@ -0,0 +1,49 @@
+macro(doxygen_exclude FOLDER)
+    set(DOXYGEN_EXCLUDE "${DOXYGEN_EXCLUDE} ${CMAKE_CURRENT_SOURCE_DIR}/src/Jatta/${FOLDER}")
+endmacro()
+
+if(DOXYGEN_ENABLE)
+
+    # Find the doxygen executable
+    find_package(Doxygen)
+    if(DOXYGEN_FOUND)
+        # Control doxygen output messages
+        set(DOXYGEN_SHOW_WARNINGS "NO")
+        set(DOXYGEN_QUIET "YES")
+        if(DOXYGEN_WARNINGS)
+            set(DOXYGEN_SHOW_WARNINGS "YES")
+            set(DOXYGEN_QUIET "NO")
+        endif()
+
+        # Use a different doxyfile depending on the doxygen version available
+        if(${DOXYGEN_VERSION} VERSION_LESS 1.7)
+            set(DOXYGEN_IN "Doxyfile-1.6.in")
+        elseif(${DOXYGEN_VERSION} VERSION_GREATER 1.6.1)
+            set(DOXYGEN_IN "Doxyfile.in")
+        else()
+            message(SEND_ERROR "Invalid Doxygen version. Requires version 1.6.1 or higher.  Set DOXYGEN_ENABLE to OFF to disable this error.")
+        endif()
+
+        # Make sure the doxygen version is compatible
+        if(NOT "${DOXYGEN_IN}" STREQUAL "")
+            # Setup doxygen defines
+            set(DOXYGEN_DEFINES "")
+
+            # Setup doxygen excludes
+            doxygen_exclude(Assimp)
+            doxygen_exclude(PortAudio)
+            doxygen_exclude(OpenGL)
+
+            # Configure the doxygen file
+            configure_file(${CMAKE_CURRENT_SOURCE_DIR}/resources/${DOXYGEN_IN} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile @ONLY)
+
+            # Add a build step to build doxygen
+            add_custom_target(doc ALL
+                ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
+                WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+                COMMENT "Generating API documentation with Doxygen" VERBATIM)
+        endif()
+    endif()
+else()
+    set(DOXYGEN_FOUND OFF)
+endif()
