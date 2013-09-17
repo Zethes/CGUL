d70c9d3d042916a59e3245e81f42f0e24ec59ed9
Joshua Brookover <josh@jalb.me>
2013-09-17 18:27:13 -0500

    Did some cleaning up and documenting.  Added SShort, UShort, SLong, and ULong.

--- src/Jatta/Utility/Version.h
+++ src/Jatta/Utility/Version.h
@@ -10,10 +10,15 @@
 
 namespace Jatta
 {
+    // @brief Gets the Jatta version as a string.
     _JATTA_EXPORT String GetJattaVersion();
+    // @brief Gets the Jatta major version.
     _JATTA_EXPORT UInt32 GetJattaVersionMajor();
+    // @brief Gets the Jatta minor version.
     _JATTA_EXPORT UInt32 GetJattaVersionMinor();
+    // @brief Gets the Jatta patch version.
     _JATTA_EXPORT UInt32 GetJattaVersionPatch();
+    // @brief Gets the name of the compiler used to build Jatta.
     _JATTA_EXPORT String GetJattaCompilerName();
 }
 
