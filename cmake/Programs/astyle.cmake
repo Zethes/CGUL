6ff8fcb36f22e611e5a322786529c0e7c2514ea2
Joshua Brookover <josh@jalb.me>
2013-11-20 20:50:43 -0600

    Adding a special case were unix machines will enforce unix line endings. Doing this on windows is a bad idea, read the note in astyle.cmake

--- cmake/Programs/astyle.cmake
+++ cmake/Programs/astyle.cmake
@@ -1,6 +1,14 @@
 # Find the program
 find_program(astyle_EXECUTABLE astyle DOC "A tool to stylize the code.")
 
+# Enforce linux line endings if on a Unix machine
+# Note: Doing this on windows is a bad idea! Some git configurations checkout as windows line
+#       endings and check in as unix. Converting everything to unix with that configuration marks
+#       every file as a change and is a huge pain...
+if(UNIX AND NOT CYGWIN)
+    set(__LINEENDINGS "--lineend=windows")
+endif()
+
 # Add a custom target
 add_custom_target("style" COMMAND
   "${astyle_EXECUTABLE}"
@@ -8,5 +16,6 @@ add_custom_target("style" COMMAND
   ${CMAKE_CURRENT_SOURCE_DIR}/src/Jatta/*.cpp
   ${CMAKE_CURRENT_SOURCE_DIR}/src/Jatta/*.h
   ${CMAKE_CURRENT_SOURCE_DIR}/src/Jatta/*.mm
+  ${__LINEENDINGS}
   VERBATIM
 )
