fba556cdaeb1e787573897a7ba46b74a64ff954a
Joshua Brookover <josh@jalb.me>
2013-12-06 22:02:05 -0600

    Fixing up color implementation. Added Get/Set for float RGBA values.

--- src/CGUL/Images/Color.hpp
+++ src/CGUL/Images/Color.hpp
@@ -21,7 +21,7 @@ namespace CGUL
     struct Color
     {
         //! @brief Creates a color given a hue, saturation, luminance, and (optionally) alpha.
-        _CGUL_EXPORT static Color MakeHSL(UInt32 hue, Byte saturation, Byte luminance, Byte alpha = 255);
+        _CGUL_INLINE_DEFINE static Color MakeHSL(UInt32 hue, Byte saturation, Byte luminance, Byte alpha = 255);
 
         //! @brief The red value.
         Byte r;
@@ -33,20 +33,20 @@ namespace CGUL
         Byte a;
 
         //! @brief Initializes a black color (0, 0, 0, 255).
-        _CGUL_EXPORT Color();
+        _CGUL_INLINE_DEFINE Color();
         //! @brief Copies a color into a new color.
-        _CGUL_EXPORT Color(const Color& copy);
+        _CGUL_INLINE_DEFINE Color(const Color& copy);
         //! @brief Initializes a color from red, green, and blue values.
-        _CGUL_EXPORT Color(unsigned char r, unsigned char g, unsigned char b);
+        _CGUL_INLINE_DEFINE Color(unsigned char r, unsigned char g, unsigned char b);
         //! @brief Initializes a color from red, green, blue, and alpha values.
-        _CGUL_EXPORT Color(unsigned char r, unsigned char g, unsigned char b, unsigned char a);
+        _CGUL_INLINE_DEFINE Color(unsigned char r, unsigned char g, unsigned char b, unsigned char a);
 
         //! @brief Checks if two colors are the same.
-        _CGUL_EXPORT Boolean operator==(const Color& operand);
+        _CGUL_INLINE_DEFINE Boolean operator==(const Color& operand);
         //! @brief Checks if two colors are not the same.
-        _CGUL_EXPORT Boolean operator!=(const Color& operand);
+        _CGUL_INLINE_DEFINE Boolean operator!=(const Color& operand);
         //! @brief Assigns one color into another.
-        _CGUL_EXPORT Color& operator=(const Color& operand);
+        _CGUL_INLINE_DEFINE Color& operator=(const Color& operand);
         //! @brief Operator to output this color into an output stream.
         friend std::ostream& operator<<(std::ostream& stream, const Color& color)
         {
@@ -56,10 +56,20 @@ namespace CGUL
 
         //! @brief Calculates RGB values from 0.0f to 1.0f and returns a Vector3 with the result.
         template< typename Type >
-        _CGUL_EXPORT Vector3T< Type > ToVector3() const;
+        _CGUL_INLINE_DEFINE Vector3T< Type > ToVector3() const;
         //! @brief Calculates RGBA values from 0.0f to 1.0f and returns a Vector4 with the result.
         template< typename Type >
-        _CGUL_EXPORT Vector4T< Type > ToVector4() const;
+        _CGUL_INLINE_DEFINE Vector4T< Type > ToVector4() const;
+
+        _CGUL_INLINE_DEFINE void SetRed(Float32 red);
+        _CGUL_INLINE_DEFINE void SetGreen(Float32 green);
+        _CGUL_INLINE_DEFINE void SetBlue(Float32 blue);
+        _CGUL_INLINE_DEFINE void SetAlpha(Float32 alpha);
+
+        _CGUL_INLINE_DEFINE Float32 GetRed() const;
+        _CGUL_INLINE_DEFINE Float32 GetGreen() const;
+        _CGUL_INLINE_DEFINE Float32 GetBlue() const;
+        _CGUL_INLINE_DEFINE Float32 GetAlpha() const;
     };
 }
 
--- src/CGUL/Images/Color_Implement.hpp
+++ src/CGUL/Images/Color_Implement.hpp
@@ -5,6 +5,7 @@
 /** @file Color_Implement.hpp
  */
 
+#include "../Math/Math.hpp"
 #include "../Math/Vector3.hpp"
 #include "../Math/Vector4.hpp"
 
@@ -14,7 +15,7 @@
  *  @returns A vector containing the r, g, b values as floating point x, y, z.
  */
 template< typename Type >
-_CGUL_EXPORT CGUL::Vector3T< Type > CGUL::Color::ToVector3() const
+_CGUL_INLINE_IMPLEMENT CGUL::Vector3T< Type > CGUL::Color::ToVector3() const
 {
     return Vector3T< Type >(this->r / 255.0f, this->g / 255.0f, this->b / 255.0f);
 }
@@ -23,7 +24,47 @@ _CGUL_EXPORT CGUL::Vector3T< Type > CGUL::Color::ToVector3() const
  *  @returns A vector containing the r, g, b, a values as floating point x, y, z, w.
  */
 template< typename Type >
-_CGUL_EXPORT CGUL::Vector4T< Type > CGUL::Color::ToVector4() const
+_CGUL_INLINE_IMPLEMENT CGUL::Vector4T< Type > CGUL::Color::ToVector4() const
 {
     return Vector4T< Type >(this->r / 255.0f, this->g / 255.0f, this->b / 255.0f, this->a / 255.0f);
+}
+
+_CGUL_INLINE_IMPLEMENT void CGUL::Color::SetRed(Float32 red)
+{
+    this->r = (Byte)(255 * Math::Clamp(red, 0.0f, 1.0f));
+}
+
+_CGUL_INLINE_IMPLEMENT void CGUL::Color::SetGreen(Float32 green)
+{
+    this->g = (Byte)(255 * Math::Clamp(green, 0.0f, 1.0f));
+}
+
+_CGUL_INLINE_IMPLEMENT void CGUL::Color::SetBlue(Float32 blue)
+{
+    this->b = (Byte)(255 * Math::Clamp(blue, 0.0f, 1.0f));
+}
+
+_CGUL_INLINE_IMPLEMENT void CGUL::Color::SetAlpha(Float32 alpha)
+{
+    this->a = (Byte)(255 * Math::Clamp(alpha, 0.0f, 1.0f));
+}
+
+_CGUL_INLINE_IMPLEMENT CGUL::Float32 CGUL::Color::GetRed() const
+{
+    return r / 255.0f;
+}
+
+_CGUL_INLINE_IMPLEMENT CGUL::Float32 CGUL::Color::GetGreen() const
+{
+    return g / 255.0f;
+}
+
+_CGUL_INLINE_IMPLEMENT CGUL::Float32 CGUL::Color::GetBlue() const
+{
+    return b / 255.0f;
+}
+
+_CGUL_INLINE_IMPLEMENT CGUL::Float32 CGUL::Color::GetAlpha() const
+{
+    return a / 255.0f;
 }
