f50ae471101c9d548aac4968a810d0f15d318393
Joshua Brookover <josh@jalb.me>
2013-12-06 22:02:36 -0600

    Renamed RectX types to SRectX.

--- src/CGUL/Math/Vector4.hpp
+++ src/CGUL/Math/Vector4.hpp
@@ -160,9 +160,9 @@ namespace CGUL
 
     typedef Vector4T< Float32 > Vector4;
     typedef Vector4T< Float32 > Vector4D;
-    typedef Vector4T< SInt16  > Rect16;
-    typedef Vector4T< SInt32  > Rect32;
-    typedef Vector4T< SInt64  > Rect64;
+    typedef Vector4T< SInt16  > SRect16;
+    typedef Vector4T< SInt32  > SRect32;
+    typedef Vector4T< SInt64  > SRect64;
     typedef Vector4T< UInt16  > URect16;
     typedef Vector4T< UInt32  > URect32;
     typedef Vector4T< UInt64  > URect64;
