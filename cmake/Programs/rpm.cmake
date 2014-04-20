512d2938fb0e49ab7fddec267d8751668a4de0ce
Amber Thrall <amber.rose.thrall@gmail.com>
2014-04-20 13:10:15 -0700

    rpm.cmake is a linux only tool.
    Also updated Doxygen's downloads page.

--- cmake/Programs/rpm.cmake
+++ cmake/Programs/rpm.cmake
@@ -1,5 +1,7 @@
-# Make sure rpmbuild is installed
-find_program(rpmbuild_BINARY rpmbuild DOC "rpmbuild is used to build both binary and source software packages.")
+if (UNIX AND NOT APPLE AND NOT CYGWIN)
+    # Make sure rpmbuild is installed
+    find_program(rpmbuild_BINARY rpmbuild DOC "rpmbuild is used to build both binary and source software packages.")
 
-# Add custom target to cmake
-add_custom_target("rpm" COMMAND "${CMAKE_CURRENT_SOURCE_DIR}/cmake/Programs/makerpm.sh" ${CMAKE_CURRENT_BINARY_DIR} ${CGUL_VERSION})
+    # Add custom target to cmake
+    add_custom_target("rpm" COMMAND "${CMAKE_CURRENT_SOURCE_DIR}/cmake/Programs/makerpm.sh" ${CMAKE_CURRENT_BINARY_DIR} ${CGUL_VERSION})
+endif()
